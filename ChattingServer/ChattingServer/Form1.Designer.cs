2aa926936b967d5fb3c8011279624e32b498a302
DAEGU\21432669 <jsg950102@naver.com>
2022-11-16 23:00:52 +0900

    스크롤바 생성

--- ChattingServer/ChattingServer/Form1.Designer.cs
+++ ChattingServer/ChattingServer/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.textBoxLog.Multiline = true;
             this.textBoxLog.Name = "textBoxLog";
             this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBoxLog.Size = new System.Drawing.Size(622, 531);
             this.textBoxLog.TabIndex = 0;
             // 
